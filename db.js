189852b865e16c217501967ef950255343886153
Arif shahzad <shahzadarif680@gmail.com>
2022-11-03 01:00:53 +0500

    updated database

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
+  host: 'awseb-e-hs73ecx9rn-stack-awsebrdsdatabase-zvb6iuvugkzx.cathhnhlauwn.us-west-2.rds.amazonaws.com',
+  user: 'ArifShahzad',
+  password: 'Arif!0300',
   database: 'user'
 });
 
